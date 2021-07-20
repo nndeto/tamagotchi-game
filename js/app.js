e6d291382718fdc270240e3540f6bd09eb206ede
nndeto <n_ndeto@yahoo.com>
2021-07-20 19:37:14 -0400

    created global variables

---
+++ js/app.js
@@ -0,0 +1,10 @@
+//creating global variables to pass into things later on
+let characterStatus = "";
+let chosen = "";
+let decideAge = Math.floor(Math.random()*99);
+let hunger = 1;
+let boredome = 1;
+let sleepiness = 1;
+let boredInterval = null;
+let hungerInterval = null;
+let sleepInterval = null;
