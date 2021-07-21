23c52278583470cd1be1706e7daa8c38b9af2641
nndeto <n_ndeto@yahoo.com>
2021-07-21 14:24:50 -0400

    finished scope

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 #Scope
+
 #User Stories
 ##Who is the user?
 
+
 #Wireframes
 ![Tamagotchi Mockup](Tamagotchi Mockup.png)
+
 #Triumphs
 #Challenges
 #Code Snippets
--- js/app.js
+++ js/app.js
@@ -5,9 +5,13 @@ let decideAge = Math.floor(Math.random()*99);
 let hungerValue = 1;
 let boredomValue = 1;
 let sleepinessValue = 1;
-let boredInterval = null;
 let hungerInterval = null;
+let boredInterval = null;
 let sleepInterval = null;
+const startButton = document.getElementById("start");
+const feedButton = document.getElementById("feed");
+const playButton = document.getElementById("play");
+const sleepButton = document.getElementById("sleep");
 
 //creating global functions
 //prompts user to choose a name
@@ -16,21 +20,32 @@ function pickName() {
 }
 
 //creates new character class
-let newCharacter = null; //have it empty so that after i create a new character, i can assign the new characters value here
+let newCharacter = null; //have it empty so that after i create a new character, 
+    //i can assign the new characters value here
 function generateCharacter() {
     newCharacter = new Tamagotchi(chosenName, decideAge, hungerValue, boredomValue, sleepinessValue);
     return newCharacter;
     //newCharacter = instance;
 }
 
-//starts game once page loads
-function startGame() {
+//this allows the user to set a name, sets the character status to alive and creates
+//the new character object
+function createCharacter() {
     characterStatus = "alive";
     pickName();
     generateCharacter();
-    console.log("i created something");
 }
 
+//this function starts my interval timers for the game starting
+//will eventually need to put display messages and whatnot within this function
+function startGame() {
+    hungerInterval = setInterval(function() {
+        console.log("hello")
+        newCharacter.increaseHunger()
+    }, 3000)
+    //boredInterval = if you get above working, pull in here and change values
+    //sleepInterval =
+}
 //creating Tamagotchi Class
 //come back and add methods for: age change, intervals, poiont changes
 class Tamagotchi {
@@ -41,7 +56,15 @@ class Tamagotchi {
         this.boredom = boredom;
         this.sleepiness = sleepiness;
     }
+    increaseHunger() {
+        this.hunger++
+        console.log(this.hunger)
+        //this is where i want my display of hunger to change as well
+    }
+
 }
 
 //addEventListeners
-window.addEventListener("load", startGame)
+window.addEventListener("load", createCharacter);
+startButton.addEventListener("click", startGame);
+
