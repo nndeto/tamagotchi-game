f6abfd87470deb36e49604f11fe31da4349f6006
nndeto <n_ndeto@yahoo.com>
2021-07-20 20:05:58 -0400

    created global functions, classes, and other global scope items

--- js/app.js
+++ js/app.js
@@ -1,10 +1,47 @@
 //creating global variables to pass into things later on
 let characterStatus = "";
-let chosen = "";
+let chosenName = "";
 let decideAge = Math.floor(Math.random()*99);
-let hunger = 1;
-let boredome = 1;
-let sleepiness = 1;
+let hungerValue = 1;
+let boredomValue = 1;
+let sleepinessValue = 1;
 let boredInterval = null;
 let hungerInterval = null;
 let sleepInterval = null;
+
+//creating global functions
+//prompts user to choose a name
+function pickName() {
+    chosenName = prompt("What would you like your Tamagotchi name to be?");
+}
+
+//creates new character class
+let newCharacter = null; //have it empty so that after i create a new character, i can assign the new characters value here
+function generateCharacter() {
+    newCharacter = new Tamagotchi(chosenName, decideAge, hungerValue, boredomValue, sleepinessValue);
+    return newCharacter;
+    //newCharacter = instance;
+}
+
+//starts game once page loads
+function startGame() {
+    characterStatus = "alive";
+    pickName();
+    generateCharacter();
+    console.log("i created something");
+}
+
+//creating Tamagotchi Class
+//come back and add methods for: age change, intervals, poiont changes
+class Tamagotchi {
+    constructor(name, age, hunger, boredom, sleepiness) {
+        this.name = name;
+        this.age = age;
+        this.hunger = hunger;
+        this.boredom = boredom;
+        this.sleepiness = sleepiness;
+    }
+}
+
+//addEventListeners
+window.addEventListener("load", startGame)
